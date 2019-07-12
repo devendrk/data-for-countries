d04acba4a2d170bebc3da229a8b0c219da328624
devendrk <katwaldevendra@hotmail>
2019-07-12 23:32:18 +0300

    consolelog deleted

--- src/Components/WeatherDetails.js
+++ src/Components/WeatherDetails.js
@@ -1,10 +1,6 @@
 import React from 'react'
 
 export default function WeatherDetails({ countryCapital, temperature, wind }) {
-  console.log('temp', temperature)
-  console.log('wind', wind)
-  console.log('cap', countryCapital)
-
   return (
     <table class="table table-sm">
       <thead>
