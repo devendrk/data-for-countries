63d24ebb9716e9d648ea0b95975d31104c87246e
devendrk <katwaldevendra@hotmail>
2019-07-12 15:36:57 +0300

    build app

---
+++ build/precache-manifest.b8c3006e0a2fd9996c45152e542a7a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c255d713f73a840643ef7a689ef60b",
+    "url": "/data-for-countries/index.html"
+  },
+  {
+    "revision": "e83848cafa9d868278b5",
+    "url": "/data-for-countries/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "87f3b962c357b72e3090",
+    "url": "/data-for-countries/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e83848cafa9d868278b5",
+    "url": "/data-for-countries/static/js/2.9db9f372.chunk.js"
+  },
+  {
+    "revision": "87f3b962c357b72e3090",
+    "url": "/data-for-countries/static/js/main.b6a9a204.chunk.js"
+  },
+  {
+    "revision": "cff361559aeca980c9d9",
+    "url": "/data-for-countries/static/js/runtime~main.76cff5e8.js"
+  }
+]);
