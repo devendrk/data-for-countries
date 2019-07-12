d8973be76cc12e5ab2ee2e3f8a7d2f3e2d1371fe
devendrk <katwaldevendra@hotmail>
2019-07-12 15:33:39 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App';
-
-import 'bootstrap/dist/css/bootstrap.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
